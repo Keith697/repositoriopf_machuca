057a6f39b940d30634349a7edd707db5e0bae6c6
ASUS <ASUS@KEITHMT.bbrouter>
2025-01-23 22:46:31 -0500

    Manifest-Version: 1.0 X-COMMENT: Main-Class will be added automatically by build

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -6,7 +6,7 @@ public class JavaApplication6 {
   public static void main(String[] args){
      //declarar variables
      int vent1,vent2,vent3;
-     double sueldom,pagom,comis;
+     double sueldom,pagom,comis,descuento;
      String vendedor;
      //crear un objeto de lectura de datos
      Scanner lectura = new Scanner(System.in);
@@ -15,6 +15,8 @@ public class JavaApplication6 {
      vendedor=lectura.next();
      System.out.print("Sueldo Mensual: ");
      sueldom=lectura.nextDouble();
+     System.out.print("Descuento: ");
+     descuento=lectura.nextDouble();
      System.out.print("Venta 1: ");
      vent1=lectura.nextInt();
      System.out.print("Venta 2: ");
@@ -23,10 +25,10 @@ public class JavaApplication6 {
      vent3=lectura.nextInt();
      // proceso de datos
      comis=(vent1+vent2+vent3)*0.10;
-     pagom=sueldom+comis;
+     pagom=sueldom+comis-descuento;
      //Salida de Datos     
      System.out.println("La comision de la venta es: " + comis);
-     System.out.println(" El sueldo mend+sual del Vendedor es: " + pagom);
+     System.out.println(" El sueldo mensual del Vendedor es: " + pagom);
             
   }  
 }
